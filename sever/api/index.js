868570472114a0705035a4065939c7003aa71fe3
Teo Bay Bong <phamanh9296@icloud.com>
2021-06-01 16:00:03 +0900

    change upload file

--- sever/api/index.js
+++ sever/api/index.js
@@ -18,7 +18,7 @@ db.connect();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images")
+    cb(null, "public/images")
   },
   filename: (req, file, cb) => {
     cb(null, req.body.name);
